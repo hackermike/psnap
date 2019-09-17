539920cd1c9a08f8eeb200de3ea9ab947b95ed03
Mike Frandsen <mfrandsen@figure.com>
2019-09-17 13:51:38 -0600

    Fix github.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='psnap',  
-     version='0.1',
+     version='0.2',
      scripts=['scripts/psnap', 'scripts/psnap_example'] ,
      author="Michael Frandsen",
      author_email="wfrandsen@gmail.com",
      description="Snapshot script output to json and expand keywords in source file.",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/mikefrandsen/psnap",
+     url="https://github.com/hackermike/psnap",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
